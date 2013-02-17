6a6a5a79210f788ca5cf249297d12ec0151a1380
aufder <Alan.Aufderheide@gmail.com>
2013-02-17 01:16:19 -0500

    Added new files.

---
+++ ListSelector.py
@@ -0,0 +1,75 @@
+# ListSelector.py
+#
+# Created by Alan Aufderheide, February 2013
+#
+# Given a list of items in the passed list,
+# allow quick access by picking letters progressively.
+# Uses up/down to go up and down where cursor is.
+# Move left/right to further filter to quickly get to item.
+# Still need to do case insensitive, and sort.
+from time import sleep
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
+class ListSelector:
+    def __init__(self, theList, theLcd):
+        self.list = []
+        for item in theList:
+            if isinstance(item, basestring):
+                self.list.append(item)
+            else:
+                self.list.append(item[0])
+        self.lcd = theLcd
+
+    def Pick(self):
+        sleep(0.5)
+        curitem = 0
+        curlen = 1
+        self.lcd.clear()
+        self.lcd.message(self.list[curitem])
+        self.lcd.home()
+        self.lcd.blink()
+        self.lcd.setCursor(0,0)
+        while 1:
+            if self.lcd.buttonPressed(self.lcd.SELECT):
+                sleep(0.5)
+                break
+            if self.lcd.buttonPressed(self.lcd.UP):
+                tempitem = curitem
+                prevstr = self.list[tempitem][:curlen]
+                while tempitem > 0 and self.list[tempitem-1][:curlen-1] == self.list[curitem][:curlen-1] and self.list[tempitem][:curlen] >= prevstr:
+                    tempitem -= 1
+                curitem = tempitem
+                # overwrite message, uses spaces to clear previous entries
+                self.lcd.home()
+                self.lcd.message(self.list[curitem]+'                ')
+                self.lcd.setCursor(curlen-1,0)
+                sleep(0.5)
+            if self.lcd.buttonPressed(self.lcd.DOWN):
+                nextstr = self.list[curitem][:curlen-1]+chr(ord(self.list[curitem][curlen-1])+1)
+                tempitem = curitem
+                while tempitem+1 < len(self.list) and self.list[tempitem+1][:curlen-1] == self.list[curitem][:curlen-1] and self.list[tempitem] < nextstr:
+                    tempitem += 1
+                if tempitem < len(self.list):
+                    curitem = tempitem
+                # overwrite message, uses spaces to clear previous entries
+                self.lcd.home()
+                self.lcd.message(self.list[curitem]+'                ')
+                self.lcd.setCursor(curlen-1,0)
+                sleep(0.5)
+            if self.lcd.buttonPressed(self.lcd.RIGHT):
+                if curlen < len(self.list[curitem]):
+                    curlen += 1
+                self.lcd.setCursor(curlen-1,0)
+                self.lcd.blink()
+                sleep(0.5)
+            if self.lcd.buttonPressed(self.lcd.LEFT):
+                if curlen > 1:
+                    curlen -= 1
+                self.lcd.setCursor(curlen-1,0)
+                self.lcd.blink()
+                sleep(0.5)
+
+        self.lcd.setCursor(0,0)
+        self.lcd.noBlink()
+        return curitem
+
---
+++ lcdmenu.py
@@ -0,0 +1,405 @@
+#!/usr/bin/python
+#
+# Created by Alan Aufderheide, February 2013
+#
+# This provides a menu driven application using the LCD Plates
+# from Adafruit Electronics.
+
+import commands
+from string import split
+from time import sleep, strftime, localtime
+from xml.dom.minidom import *
+from Adafruit_I2C import Adafruit_I2C
+from Adafruit_MCP230xx import Adafruit_MCP230XX
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from ListSelector import ListSelector
+
+import smbus
+
+configfile = 'lcdmenu.xml'
+# set DEBUG=1 for print debug statements
+DEBUG = 0
+DISPLAY_ROWS = 2
+DISPLAY_COLS = 16
+
+# set busnum param to the correct value for your pi
+lcd = Adafruit_CharLCDPlate(busnum = 1)
+# in case you add custom logic to lcd to check if it is connected (useful)
+#if lcd.connected == 0:
+#    quit()
+
+lcd.begin(DISPLAY_COLS, DISPLAY_ROWS)
+lcd.backlight(lcd.OFF)
+
+# commands
+def DoQuit():
+    lcd.clear()
+    lcd.message('Are you sure?\nPress Sel for Y')
+    while 1:
+        if lcd.buttonPressed(lcd.LEFT):
+            break
+        if lcd.buttonPressed(lcd.SELECT):
+            lcd.clear()
+            lcd.backlight(lcd.OFF)
+            quit()
+        sleep(0.25)
+
+def DoShutdown():
+    lcd.clear()
+    lcd.message('Are you sure?\nPress Sel for Y')
+    while 1:
+        if lcd.buttonPressed(lcd.LEFT):
+            break
+        if lcd.buttonPressed(lcd.SELECT):
+            lcd.clear()
+            lcd.backlight(lcd.OFF)
+            commands.getoutput("sudo shutdown -h now")
+            quit()
+        sleep(0.25)
+
+def LcdOff():
+    lcd.backlight(lcd.OFF)
+
+def LcdOn():
+    lcd.backlight(lcd.ON)
+
+def LcdRed():
+    lcd.backlight(lcd.RED)
+
+def LcdGreen():
+    lcd.backlight(lcd.GREEN)
+
+def LcdBlue():
+    lcd.backlight(lcd.BLUE)
+
+def LcdYellow():
+    lcd.backlight(lcd.YELLOW)
+
+def LcdTeal():
+    lcd.backlight(lcd.TEAL)
+
+def LcdViolet():
+    lcd.backlight(lcd.VIOLET)
+
+def ShowDateTime():
+    if DEBUG:
+        print('in ShowDateTime')
+    lcd.clear()
+    while not(lcd.buttonPressed(lcd.LEFT)):
+        sleep(0.25)
+        lcd.home()
+        lcd.message(strftime('%a %b %d %Y\n%I:%M:%S %p', localtime()))
+    
+def SetDateTime():
+    if DEBUG:
+        print('in SetDateTime')
+
+def ShowIPAddress():
+    if DEBUG:
+        print('in ShowIPAddress')
+    lcd.clear()
+    lcd.message(commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:])
+    while 1:
+        if lcd.buttonPressed(lcd.LEFT):
+            break
+        sleep(0.25)
+    
+#only use the following if you find useful
+def Use10Network():
+    "Allows you to switch to a different network for local connection"
+    lcd.clear()
+    lcd.message('Are you sure?\nPress Sel for Y')
+    while 1:
+        if lcd.buttonPressed(lcd.LEFT):
+            break
+        if lcd.buttonPressed(lcd.SELECT):
+            # uncomment the following once you have a separate network defined
+            #commands.getoutput("sudo cp /etc/network/interfaces.hub.10 /etc/network/interfaces")
+            lcd.clear()
+            lcd.message('Please reboot')
+            sleep(1.5)
+            break
+        sleep(0.25)
+
+#only use the following if you find useful
+def UseDHCP():
+    "Allows you to switch to a network config that uses DHCP"
+    lcd.clear()
+    lcd.message('Are you sure?\nPress Sel for Y')
+    while 1:
+        if lcd.buttonPressed(lcd.LEFT):
+            break
+        if lcd.buttonPressed(lcd.SELECT):
+            # uncomment the following once you get an original copy in place
+            #commands.getoutput("sudo cp /etc/network/interfaces.orig /etc/network/interfaces")
+            lcd.clear()
+            lcd.message('Please reboot')
+            sleep(1.5)
+            break
+        sleep(0.25)
+
+def ShowLatLon():
+    if DEBUG:
+        print('in ShowLatLon')
+
+def SetLatLon():
+    if DEBUG:
+        print('in SetLatLon')
+    
+def SetLocation():
+    if DEBUG:
+        print('in SetLocation')
+    global lcd
+    list = []
+    # coordinates usable by ephem library, lat, lon, elevation (m)
+    list.append(['New York', '40.7143528', '-74.0059731', 9.775694])
+    list.append(['Paris', '48.8566667', '2.3509871', 35.917042])
+    selector = ListSelector(list, lcd)
+    item = selector.Pick()
+    # do something useful
+    chosen = list[item]
+
+def CompassGyroViewAcc():
+    if DEBUG:
+        print('in CompassGyroViewAcc')
+
+def CompassGyroViewMag():
+    if DEBUG:
+        print('in CompassGyroViewMag')
+
+def CompassGyroViewHeading():
+    if DEBUG:
+        print('in CompassGyroViewHeading')
+
+def CompassGyroViewTemp():
+    if DEBUG:
+        print('in CompassGyroViewTemp')
+
+def CompassGyroCalibrate():
+    if DEBUG:
+        print('in CompassGyroCalibrate')
+    
+def CompassGyroCalibrateClear():
+    if DEBUG:
+        print('in CompassGyroCalibrateClear')
+    
+def TempBaroView():
+    if DEBUG:
+        print('in TempBaroView')
+
+def TempBaroCalibrate():
+    if DEBUG:
+        print('in TempBaroCalibrate')
+    
+def AstroViewAll():
+    if DEBUG:
+        print('in AstroViewAll')
+
+def AstroViewAltAz():
+    if DEBUG:
+        print('in AstroViewAltAz')
+    
+def AstroViewRADecl():
+    if DEBUG:
+        print('in AstroViewRADecl')
+
+def CameraDetect():
+    if DEBUG:
+        print('in CameraDetect')
+    
+def CameraTakePicture():
+    if DEBUG:
+        print('in CameraTakePicture')
+
+def CameraTimeLapse():
+    if DEBUG:
+        print('in CameraTimeLapse')
+
+class CommandToRun:
+    def __init__(self, myName, theCommand):
+        self.text = myName
+        self.commandToRun = theCommand
+    def Run(self):
+        self.clist = split(commands.getoutput(self.commandToRun), '\n')
+        if len(self.clist) > 0:
+            lcd.clear()
+            lcd.message(self.clist[0])
+            for i in range(1, len(self.clist)):
+                while 1:
+                    if lcd.buttonPressed(lcd.DOWN):
+                        break
+                    sleep(0.25)
+                lcd.clear()
+                lcd.message(self.clist[i-1]+'\n'+self.clist[i])          
+                sleep(0.5)
+        while 1:
+            if lcd.buttonPressed(lcd.LEFT):
+                break
+
+class Widget:
+    def __init__(self, myName, myFunction):
+        self.text = myName
+        self.function = myFunction
+        
+class Folder:
+    def __init__(self, myName, myParent):
+        self.text = myName
+        self.items = []
+        self.parent = myParent
+
+def ProcessNode(currentNode, currentItem):
+    children = currentNode.childNodes
+
+    for child in children:
+        if isinstance(child, xml.dom.minidom.Element):
+            if child.tagName == 'folder':
+                thisFolder = Folder(child.getAttribute('text'), currentItem)
+                currentItem.items.append(thisFolder)
+                ProcessNode(child, thisFolder)
+            if child.tagName == 'widget':
+                thisWidget = Widget(child.getAttribute('text'), child.getAttribute('function'))
+                currentItem.items.append(thisWidget)
+            if child.tagName == 'run':
+                thisCommand = CommandToRun(child.getAttribute('text'), child.firstChild.data)
+                currentItem.items.append(thisCommand)
+
+class Display:
+    def __init__(self, folder):
+        self.curFolder = folder
+        self.curTopItem = 0
+        self.curSelectedItem = 0
+    def display(self):
+        if self.curTopItem > len(self.curFolder.items) - DISPLAY_ROWS:
+            self.curTopItem = len(self.curFolder.items) - DISPLAY_ROWS
+        if self.curTopItem < 0:
+            self.curTopItem = 0
+        if DEBUG:
+            print('------------------')
+        str = ''
+        for row in range(self.curTopItem, self.curTopItem+DISPLAY_ROWS):
+            if row > self.curTopItem:
+                str += '\n'
+            if row < len(self.curFolder.items):
+                if row == self.curSelectedItem:
+                    cmd = '-'+self.curFolder.items[row].text
+                    if len(cmd) < 16:
+                        for row in range(len(cmd), 16):
+                            cmd += ' '
+                    if DEBUG:
+                        print('|'+cmd+'|')
+                    str += cmd
+                else:
+                    cmd = ' '+self.curFolder.items[row].text
+                    if len(cmd) < 16:
+                        for row in range(len(cmd), 16):
+                            cmd += ' '
+                    if DEBUG:
+                        print('|'+cmd+'|')
+                    str += cmd
+        if DEBUG:
+            print('------------------')
+        lcd.home()
+        lcd.message(str)
+
+    def update(self, command):
+        if DEBUG:
+            print('do',command)
+        if command == 'u':
+            self.up()
+        elif command == 'd':
+            self.down()
+        elif command == 'r':
+            self.right()
+        elif command == 'l':
+            self.left()
+        elif command == 's':
+            self.select()
+    def up(self):
+        if self.curSelectedItem == 0:
+            return
+        elif self.curSelectedItem > self.curTopItem:
+            self.curSelectedItem -= 1
+        else:
+            self.curTopItem -= 1
+            self.curSelectedItem -= 1
+    def down(self):
+        if self.curSelectedItem+1 == len(self.curFolder.items):
+            return
+        elif self.curSelectedItem < self.curTopItem+DISPLAY_ROWS-1:
+            self.curSelectedItem += 1
+        else:
+            self.curTopItem += 1
+            self.curSelectedItem += 1
+    def left(self):
+        if isinstance(self.curFolder.parent, Folder):
+            # find the current in the parent
+            itemno = 0
+            index = 0
+            for item in self.curFolder.parent.items:
+                if self.curFolder == item:
+                    if DEBUG:
+                        print('foundit')
+                    index = itemno
+                else:
+                    itemno += 1
+            if index < len(self.curFolder.parent.items):
+                self.curFolder = self.curFolder.parent
+                self.curTopItem = index
+                self.curSelectedItem = index
+            else:
+                self.curFolder = self.curFolder.parent
+                self.curTopItem = 0
+                self.curSelectedItem = 0
+    def right(self):
+        if isinstance(self.curFolder.items[self.curSelectedItem], Folder):
+            self.curFolder = self.curFolder.items[self.curSelectedItem]
+            self.curTopItem = 0
+            self.curSelectedItem = 0
+        elif isinstance(self.curFolder.items[self.curSelectedItem], Widget):
+            if DEBUG:
+                print('eval', self.curFolder.items[self.curSelectedItem].function)
+            eval(self.curFolder.items[self.curSelectedItem].function+'()')
+        elif isinstance(self.curFolder.items[self.curSelectedItem], CommandToRun):
+            self.curFolder.items[self.curSelectedItem].Run()
+
+    def select(self):
+        if DEBUG:
+            print('check widget')
+        if isinstance(self.curFolder.items[self.curSelectedItem], Widget):
+            if DEBUG:
+                print('eval', self.curFolder.items[self.curSelectedItem].function)
+            eval(self.curFolder.items[self.curSelectedItem].function+'()')
+
+# now start things up
+uiItems = Folder('root','')
+
+dom = parse(configfile) # parse an XML file by name
+
+top = dom.documentElement
+
+ProcessNode(top, uiItems)
+
+display = Display(uiItems)
+display.display()
+
+while 1:
+	if (lcd.buttonPressed(lcd.LEFT)):
+		display.update('l')
+		display.display()
+
+	if (lcd.buttonPressed(lcd.UP)):
+		display.update('u')
+		display.display()
+
+	if (lcd.buttonPressed(lcd.DOWN)):
+		display.update('d')
+		display.display()
+
+	if (lcd.buttonPressed(lcd.RIGHT)):
+		display.update('r')
+		display.display()
+
+	if (lcd.buttonPressed(lcd.SELECT)):
+		display.update('s')
+		display.display()
+
